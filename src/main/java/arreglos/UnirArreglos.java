8bbacc5734f3651684028112b3da2b5820526130
amarianjel <abrm.engineer@gmail.com>
2024-08-05 15:42:21 -0400

    Arch - Actualizando arreglos

--- src/main/java/arreglos/UnirArreglos.java
+++ src/main/java/arreglos/UnirArreglos.java
@@ -17,7 +17,7 @@ public class UnirArreglos {
         
         System.out.println(Arrays.toString(arregloUnido));*/
         // Crear dos BDD
-        String[] nombres = {"Juan", "Alberto", "Esperanza", "Amaia", "Alba"};
+        /*String[] nombres = {"Juan", "Alberto", "Esperanza", "Amaia", "Alba"};
         String[] apellidos = {"Irribarra", "Pozo", "Marianjel", "Guitierrez", "Letelier"};
         String[] arregloUnido = new String[nombres.length + apellidos.length];
         
@@ -27,12 +27,38 @@ public class UnirArreglos {
             arregloUnido[indice++] = nombres[i];
         }
         for (int i = 0; i < apellidos.length; i++) {
-                        System.out.println(indice + " " +  i);
+            System.out.println(indice + " " +  i);
             arregloUnido[indice++] = apellidos[i];
         }
+        System.out.println(Arrays.toString(arregloUnido));*/
         
-        
-        
-        System.out.println(Arrays.toString(arregloUnido));
+        // README: 2-. Nombre - Apellido
+        /*String[] nombres = {"Juan", "Alberto", "Esperanza", "Amaia", "Alba"};
+        String[] apellidos = {"Irribarra", "Pozo", "Marianjel", "Guitierrez", "Letelier"};
+        String[] arregloUnido = new String[nombres.length + apellidos.length];
+
+        System.out.println("Nombre - Apellido");
+        for(int i = 0; i < nombres.length; i++){
+            System.out.println(nombres[i] + " " + apellidos[i]);
+        }*/
+
+        //README: 3-. Guardar en el arreglo y mostrarlo
+        String[] nombres = {"Juan", "Alberto", "Esperanza", "Amaia", "Alba"};
+        String[] apellidos = {"Irribarra", "Pozo", "Marianjel", "Guitierrez", "Letelier"};
+        String[] arregloUnido = new String[nombres.length + apellidos.length];
+
+        int indice = 0;
+        for (int i = 0; i < nombres.length; i++) {
+            arregloUnido[indice++] = nombres[i];
+            arregloUnido[indice++] = apellidos[i];
+        }
+
+        for(int i = 0; i < arregloUnido.length; i++){
+            if( i % 2 == 0){
+                System.out.print(arregloUnido[i] + " ");
+            }else{
+                System.out.println(arregloUnido[i]);
+            }
+        }
     }
 }
